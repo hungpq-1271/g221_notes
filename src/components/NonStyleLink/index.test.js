1b51f59f2b1638d82465d3bffcb731858e61b5da
hungpq96 <quanghwng22@gmail.com>
2019-09-03 20:47:50 +0700

    Update snapshots

---
+++ src/components/NonStyleLink/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import NonStyleLink from '../NonStyleLink'
+
+describe('<NonStyleLink />', () => {
+  it('should render and match the snapshot', () => {
+    const { container } = render(<NonStyleLink />)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
